fe562f40039e95c0db38939354281b1942abf85a
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2025-11-06 15:10:55 -0500

    Adjust basePose and offsets in config.js

--- docs/config/config.js
+++ docs/config/config.js
@@ -83,13 +83,13 @@ window.CONFIG = {
       parts: { hitbox:{ w:80, h:110, r:60, torsoAttach:{ nx:0.4, ny:0.6 } }, torso:{ len:40 }, arm:{ upper:30, lower:40 }, leg:{ upper:30, lower:30 }, head:{ neck:10, radius:12 } },
       hierarchy: { legsFollowTorsoRotation: false },
       ik: { calvesOnly: true },
-      basePose: { torso:0, lShoulder:-90, lElbow:0, rShoulder:-90, rElbow:0, lHip:90, lKnee:0, rHip:90, rKnee:0 },
+      basePose: { torso:0, lShoulder:0, lElbow:0, rShoulder:0, rElbow:0, lHip:90, lKnee:0, rHip:90, rKnee:0 },
       limits: { torso:{ absMin:-45, absMax:90 }, shoulder:{ relMin:-360, relMax:-90 }, elbow:{ relMin:-170, relMax:0 }, hip:{ absMin:90, absMax:210 }, knee:{ relMin:0, relMax:170 } },
       offsets: {
-        torso: { origin:{ax:0, ay:0}, shoulder:{ax:-8, ay:-5}, hip:{ax:0, ay:0}, neck:{ax:0, ay:0} },
+        torso: { origin:{ax:0, ay:0}, shoulder:{ax:8, ay:5}, hip:{ax:0, ay:0}, neck:{ax:0, ay:0} },
         arm: { upper:{ origin:{ax:0, ay:0}, elbow:{ax:0, ay:0} }, lower:{ origin:{ax:0, ay:0} } },
         leg: { upper:{ origin:{ax:0, ay:0}, knee:{ax:0, ay:0}  }, lower:{ origin:{ax:0, ay:0} } },
-        head:{ origin:{ax:-1, ay:6} }
+        head:{ origin:{ax:1, ay:-6} }
       },
       sprites: {
         torso: { url: "./assets/fightersprites/tletingan/torso.png" },
