c75aee1c38b3db8faa8447e40f518155b7b6cfc2
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2025-11-08 18:28:33 -0500

    Refactor sprite transformation parameters to scale 1

--- docs/config/config.js
+++ docs/config/config.js
@@ -373,12 +373,12 @@ window.CONFIG = {
           widthFactor: { torso:1.0, armUpper:1.0, armLower:1.0, legUpper:1.0, legLower:1.0, head:1.0 },
           xformUnits: "percent",
           xform: {
-            torso:    { ax:-0.5,  ay:-0.00, scaleX:4.50, scaleY:4.50, rotDeg:180 },
-            head:     { ax:-1.40, ay:-0.20, scaleX:4.50, scaleY:4.50, rotDeg:180 },
-            armUpper: { ax:0.00,  ay:0.00,  scaleX:3.00, scaleY:3.00, rotDeg:0 },
-            armLower: { ax:0.00,  ay:0.00,  scaleX:2.00, scaleY:2.00, rotDeg:0 },
-            legUpper: { ax:-0.10, ay:0.10,  scaleX:2.0,  scaleY:2.0,  rotDeg:0 },
-            legLower: { ax:-0.2,  ay:0.02,  scaleX:2,    scaleY:2.00, rotDeg:-10 }
+            torso:    { ax:0,  ay:0, scaleX:1, scaleY:1, rotDeg:180 },
+            head:     { ax:0, ay:0, scaleX:1, scaleY:1, rotDeg:180 },
+            armUpper: { ax:0.00,  ay:0.00,  scaleX:1, scaleY:1, rotDeg:0 },
+            armLower: { ax:0.00,  ay:0.00,  scaleX:1, scaleY:1, rotDeg:0 },
+            legUpper: { ax:-0.10, ay:0.10,  scaleX:1,  scaleY:1,  rotDeg:0 },
+            legLower: { ax:-0.0,  ay:0.00,  scaleX:1,    scaleY:1, rotDeg:0 }
           }
       }
     }
