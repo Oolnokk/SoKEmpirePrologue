efe086eaf97bd5cbe91a88df6908aaa87d09eacc
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2025-11-09 15:34:54 -0500

    Fix saturation value for legs cosmetic

--- docs/config/config.js
+++ docs/config/config.js
@@ -683,7 +683,7 @@ window.CONFIG = {
       cosmetics: {
         slots: {
           hat: { id: 'basic_headband', hsv: { h: -20, s: 0.2, v: 0 } },
-          legs: { id: 'basic_pants', hsv: { h: 5, s: -0.1, v: 0.15 } }
+          legs: { id: 'basic_pants', hsv: { h: 5, s: 100, v: 0.15 } }
         }
       }
     },
