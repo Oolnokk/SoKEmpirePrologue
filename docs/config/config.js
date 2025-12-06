1a37ae75277e48cdc3ca11e535ddcf857deb9c77
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2025-12-06 18:47:09 -0500

    Modify spriteStyle parameters for torso and head
    Adjusted scaleY and ay values for torso and head.

--- docs/config/config.js
+++ docs/config/config.js
@@ -1168,8 +1168,8 @@ window.CONFIG = {
           widthFactor: { torso:1.0, armUpper:1.0, armLower:1.0, legUpper:1.0, legLower:1.0, head:1.0 },
           xformUnits: "percent",
           xform: {
-            torso:    { ax:0.2,  ay:-0.1, scaleX:0.8, scaleY:1.5, rotDeg:90 },
-            head:     { ax:-0.15, ay:0.2, scaleX:1, scaleY:1.2, rotDeg:90 },
+            torso:    { ax:0.2,  ay:-0.1, scaleX:0.8, scaleY:1.25, rotDeg:90 },
+            head:     { ax:-0.15, ay:0.1, scaleX:1, scaleY:1.2, rotDeg:90 },
             armUpper: { ax:-0.2,  ay:0,  scaleX:1.6, scaleY:2.8, rotDeg:-0 },
             armLower: { ax:0.35,  ay:0,  scaleX:1.7, scaleY:2.1, rotDeg:-0 },
             legUpper: { ax:-0.10, ay:0,  scaleX:1.7, scaleY:2.75,  rotDeg:0 },
