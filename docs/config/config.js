72c0ea890ed79137785ac39a111fc88ef2d9595f
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2025-12-06 22:34:31 -0500

    Update config.js

--- docs/config/config.js
+++ docs/config/config.js
@@ -1082,7 +1082,7 @@ window.CONFIG = {
           head: { relMin:75, relMax:100 }
         },
         headTracking: {
-          offsetDeg: -90
+          offsetDeg: 0
         },
       offsets: {
         torso: { origin:{ax:0, ay:0}, shoulder:{ax:-8, ay:-5}, hip:{ax:0, ay:0}, neck:{ax:0, ay:0} },
@@ -1146,7 +1146,7 @@ window.CONFIG = {
       ik: { calvesOnly: true },
       limits: { torso:{ absMin:-45, absMax:90 }, shoulder:{ relMin:-360, relMax:-90 }, elbow:{ relMin:-170, relMax:0 }, hip:{ absMin:90, absMax:210 }, knee:{ relMin:0, relMax:170 }, head:{ relMin:75, relMax:100 } },
       headTracking: {
-        offsetDeg: -90
+        offsetDeg: 0
       },
       offsets: {
         torso: { origin:{ax:0, ay:0}, shoulder:{ax:-8, ay:-5}, hip:{ax:0, ay:0}, neck:{ax:0, ay:0} },
@@ -1205,7 +1205,7 @@ window.CONFIG = {
       ik: { calvesOnly: true },
       limits: { torso:{ absMin:-45, absMax:90 }, shoulder:{ relMin:-360, relMax:-90 }, elbow:{ relMin:-170, relMax:0 }, hip:{ absMin:90, absMax:210 }, knee:{ relMin:0, relMax:170 }, head:{ relMin:75, relMax:100 } },
       headTracking: {
-        offsetDeg: -90
+        offsetDeg: 0
       },
       offsets: {
         torso: { origin:{ax:0, ay:0}, shoulder:{ax:-8, ay:-5}, hip:{ax:0, ay:0}, neck:{ax:0, ay:0} },
