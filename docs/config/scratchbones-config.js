d3a91895cf9a50d2abe2c5f36515bfae1467f0f8
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-04-13 18:03:33 -0400

    Rename scratchbones portrait config to generic scratchbones config

---
+++ docs/config/scratchbones-config.js
@@ -0,0 +1,5 @@
+window.SCRATCHBONES_CONFIG = window.SCRATCHBONES_CONFIG || {};
+window.SCRATCHBONES_CONFIG.portrait = {
+  assetBase: './docs/assets/',
+  configBase: './docs/config/'
+};
--- docs/config/scratchbones-portrait-config.js
+++
@@ -1,4 +0,0 @@
-window.SCRATCHBONES_PORTRAIT_CONFIG = {
-  assetBase: './docs/assets/',
-  configBase: './docs/config/'
-};
